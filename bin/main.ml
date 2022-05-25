0d5c229427a0355cbbde5e324105f5791f2c8fbd
Michael Fragano <mjf347@cornell.edu>
2022-05-25 12:53:26 -0400

    added start prompt

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
 (**Test?*)
+let rec run () =
+  print_endline "What thing do you wanna visualize? Type 'Q' to quit.\n";
+  let file = read_line() in
+  if file = "Q" then (print_endline "Thanks for visiting!\n")
+  else 
+    (print_endline "renderer is currently unimplemented\n";
+    run())
+in
+
+run()
